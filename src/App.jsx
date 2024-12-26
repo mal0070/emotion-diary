2867fa910bda45c29d92110fd814fe8d3d4c5d44
Mina Lee <mal0070@naver.com>
2024-12-27 01:21:29 +0900

    feat: implement CRUD function

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,88 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useActionData } from 'react-router-dom';
 import { Home, Diary, New, NotFound, Edit } from './pages';
 import './App.css';
 import Button from './components/Button';
 import Header from './components/Header';
+import { useReducer, useRef } from 'react';
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item)=> String(item.id) === String(action.data.id) ? action.data : item);
+    case "DELETE":
+      return state.filter((item)=> String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRdf = useRef(3);
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRdf.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content
+      }
+    })
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id
+    })
+  };
+
   return (
     <>
-      <Header title={"Header"} leftChild={<Button text={'Left'}/>} rightChild={<Button text={"Right"}/>}/>
+      <Header
+        title={'Header'}
+        leftChild={<Button text={'Left'} />}
+        rightChild={<Button text={'Right'} />}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit/>} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      </>
+    </>
   );
 }
 
